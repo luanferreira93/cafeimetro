6f2d1697f0556ee901539cdd137b200535375e6a
luanferreira93 <luancarlosjn@gmail>
2022-01-21 12:27:50 -0300

    inicio projeto/criação banco de dados

---
+++ db/cafeimetrodb.sql
@@ -0,0 +1,27 @@
+create database if not exists cafeimentro character set utf8 collate utf8_general_ci;
+
+use cafeimentro;
+
+create table tipos(
+    id integer auto_increment primary key,
+    nome varchar(25)
+) engine=InnoDB;
+
+create table cafe(
+    id integer auto_increment primary key,
+    nome varchar(25),
+    descricao varchar(125),
+    tipo_id integer,
+    constraint foreign key (tipo_id) references tipos (id)
+) engine=InnoDB;
+
+create table consumo(
+    id integer auto_increment primary key,
+    data date,
+    hora time,
+    qtd integer,
+    dia_semana varchar(7),
+    preco decimal(5,2),
+    cafe_id integer,
+    constraint foreign key (cafe_id) references cafe (id)
+)engine=InnoDB;
