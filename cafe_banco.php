5b9ccf332607e7acfcc9aebfe875d8649b5dcdd4
luanferreira93 <luanf7321@gmail.com>
2022-01-30 17:49:50 -0300

    Refatorando arquivo SQL

--- cafe_banco.php
+++ cafe_banco.php
@@ -3,33 +3,41 @@ function listacafe($conexao)
 {
     $cafes = [];
     $query = 'SELECT c.id,c.nome AS nome_cafe,c.descricao, t.nome AS nome_tipo FROM cafe c INNER JOIN tipos t ON (c.tipo_id=t.id)';
-    $resultado = mysqli_query($conexao, $query);
-    while($cafe = mysqli_fetch_array($resultado)){
+    $instrucao = $conexao->prepare($query);
+    $instrucao->execute();
+    $resultado = $instrucao->get_result();
+    while($cafe = $resultado->fetch_assoc()){
         array_push($cafes,$cafe);
     }
     return $cafes;
 }
 
 function buscaCafeID($conexao,$id){
-    $query = "SELECT id, nome, descricao, tipo_id FROM cafe WHERE id = {$id}";
-    $resultado = mysqli_query($conexao, $query);
-    return mysqli_fetch_assoc($resultado);
+    $query = "SELECT id, nome, descricao, tipo_id FROM cafe WHERE id = ?";
+    $instrucao = $conexao->prepare($query);
+    $instrucao->bind_param('i',$id);
+    $instrucao->execute();
+    $resultado = $instrucao->get_result();
+    return $resultado->fetch_assoc();
 }
 
 function alteraCafe($conexao,$id,$nome,$descricao,$tipo_id){
-    $query = "UPDATE cafe SET nome='{$nome}',descricao='{$descricao}',tipo_id={$tipo_id} WHERE id = {$id}";
-    $resultado = mysqli_query($conexao, $query);
-    return $resultado;
+    $query = "UPDATE cafe SET nome=?, descricao=?, tipo_id=? WHERE id =?";
+    $instrucao = $conexao->prepare($query);
+    $instrucao->bind_param('ssii',$nome,$descricao,$tipo_id,$id);
+    return $instrucao->execute();
 }
 
 function removeCafe($conexao,$id){
-    $query = "DELETE FROM cafe WHERE id = {$id}";
-    $resultado = mysqli_query($conexao, $query);
-    return $resultado;
+    $query = "DELETE FROM cafe WHERE id = ?";
+    $instrucao = $conexao->prepare($query);
+    $instrucao->bind_param('i',$id);
+    return $instrucao->execute();
 }
 
 function adiciona_cafe($conexao,$nome,$descricao,$tipo_id){
-    $query = "INSERT INTO cafe(nome,descricao,tipo_id) VALUES('{$nome}','${descricao}',{$tipo_id})";
-    $resultado = mysqli_query($conexao, $query);
-    return $resultado;
+    $query = "INSERT INTO cafe(nome,descricao,tipo_id) VALUES(?,?,?)";
+    $instrucao = $conexao->prepare($query);
+    $instrucao->bind_param('ssi',$nome,$descricao,$tipo_id);
+    return $instrucao->execute();
 }
