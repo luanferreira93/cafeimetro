e0e3794dc2cb0b36ae3883240ae9c8e6d6629b39
luanferreira93 <luanf7321@gmail.com>
2022-01-31 16:46:35 -0300

    Pagina de adição de tipos de café

---
+++ tipo_form_adiciona.php
@@ -0,0 +1,20 @@
+<?php
+include 'head_menu.php'
+?>
+<h1>Adiciona Tipo</h1>
+<form action="tipo_adiciona.php" method="POST">
+    <table class="table">
+        <tr>
+            <td>Tipo</td>
+            <td>
+                <input class="form-control" type="text" name="nome">
+                <tr>
+                    <td><button class="btn btn-primary" type="submit">Adicionar Tipo</button></td>
+                </tr>
+            </td>
+        </tr>
+    </table>
+</form>
+<?php
+include 'footer.php'
+?>
