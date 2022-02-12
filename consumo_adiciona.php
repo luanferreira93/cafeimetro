b20b8a9087ff08453b11ec0b55a0d292edd0d84a
luanferreira93 <luanf7321@gmail.com>
2022-02-11 22:39:37 -0300

    Validação lado servidor

--- consumo_adiciona.php
+++ consumo_adiciona.php
@@ -1,16 +1,18 @@
 <?php
+
 include 'conecta.php';
 include 'consumo_banco.php';
 include 'funcao_util.php';
 
-$data = $_POST['data'];
-$hora = $_POST['hora'];
+$data = (array_key_exists('data',$_POST) && $_POST['data'] != '')? $_POST['data'] : date('Y-m-d');
+$hora = (array_key_exists('hora',$_POST) && $_POST['hora'] != '')? $_POST['hora'] : date('Y:i');
 $cafe = $_POST['cafe'];
-$qtd = $_POST['qtd'];
-$preco = $_POST['preco'];
+$qtd = (array_key_exists('qtd',$_POST) && $_POST['qtd'] != '')? $_POST['qtd'] : 0;
+$preco = (array_key_exists('preco',$_POST) && $_POST['preco'] != '')? $_POST['preco'] : 0;
+
 $dia_semana = diaSemana($data);
+
 $adicionou = adicionaConsumo($conexao,$data,$hora,$cafe,$qtd,$preco,$dia_semana);
-echo $dia_semana;
 
 if($adicionou){
     header('Location:consumo_lista.php');
